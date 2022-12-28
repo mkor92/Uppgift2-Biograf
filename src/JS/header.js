a234d01267bff52fa4af89f46a49609e48928793
mkor92 <mkorhonen92@gmail.com>
2022-12-28 21:16:18 +0100

    hamburger menu added

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,4 +1,17 @@
 // -------------------- MOBILE MENU --------------------
-document.querySelector(".header-nav-toggle").addEventListener("click", () => {
-  document.querySelector(".header-nav").classList.toggle("open");
+
+const navToggle = document.querySelector(".header-nav-toggle");
+const backDrop = document.querySelector(".header-nav");
+
+navToggle.addEventListener("click", () => {
+  backDrop.classList.toggle("open");
+  function closeMobileMenu() {
+    window.onclick = function (event) {
+      if (event.target == backDrop) {
+        backDrop.classList.remove("open");
+      }
+    };
+  }
+
+  backDrop.addEventListener("click", closeMobileMenu);
 });
