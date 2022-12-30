38d2f9d13016df01a5f0d066c13bf6858449c288
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2022-12-30 19:50:42 +0100

    Changed font size on footer and nav menu and formated with prettier file booking.js

--- src/JS/booking.js
+++ src/JS/booking.js
@@ -2,22 +2,22 @@ import listenerMenuBtn from "./listeners.js";
 
 listenerMenuBtn();
 
+const bookingRenderContainer = document.querySelector(
+  "#booking-render-container"
+);
 
-const bookingRenderContainer = document.querySelector('#booking-render-container');
+const saveBookingInfo = () => {
+  //placeholders for variables
+  let salon = "A"; //e.target.dataset.salon
+  let title = "Terminator"; //e.target.datset.title
+  let time = "2022-12-29 kl. 20.00"; //e.target.datset.time
 
-const saveBookingInfo = () => { 
-    
-    //placeholders for variables
-    let salon = 'A'; //e.target.dataset.salon
-    let title = 'Terminator'; //e.target.datset.title
-    let time = '2022-12-29 kl. 20.00'; //e.target.datset.time
-
-    renderBookingSalonTitleDate(salon, title, time);  
-}
+  renderBookingSalonTitleDate(salon, title, time);
+};
 
 const renderBookingSalonTitleDate = (salon, title, time) => {
-    let bookingInfoContainer = document.createElement('div');
-    bookingInfoContainer.innerHTML = `
+  let bookingInfoContainer = document.createElement("div");
+  bookingInfoContainer.innerHTML = `
         <h2>salong ${salon}</h2>
         <h1>${title}</h1>
         <h3>${time}</h3>
@@ -25,9 +25,9 @@ const renderBookingSalonTitleDate = (salon, title, time) => {
             <button>Köp biljetter</button>
         </div>
     `;
-    
-    //bookingRenderContainer.appendChild(bookingInfoContainer);
-}
+
+  //bookingRenderContainer.appendChild(bookingInfoContainer);
+};
 
 // This should be triggered from an event when you click "biljetter" in the info about movie page.
 saveBookingInfo();
