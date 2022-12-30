c0f92b2a228878a0580929ea97edf0e8e1effb35
mkor92 <mkorhonen92@gmail.com>
2022-12-30 19:10:52 +0100

    in progress

--- src/JS/movies.js
+++ src/JS/movies.js
@@ -1,48 +1,7 @@
-const url = "src/filmer.json";
-const ul = document.querySelector("#movies-list");
-const list = document.createDocumentFragment();
-
-async function getMovies() {
-  try {
-    const response = await fetch(url);
-    const movies = await response.json();
-
-    listData(movies);
-  } catch (error) {
-    console.log(error);
-  }
+async function getJSON() {
+  const response = await fetch("src/movies.json");
+  const json = await response.json();
+  console.log(json);
 }
 
-function listData(movies) {
-  data.map(function (legend) {
-    const legendText = `
-    <div class="nickname">${legend.nickname}</div> 
-    `;
-    const item = document.createElement("li");
-    item.innerHTML = legendText;
-    list.appendChild(item);
-  });
-  ul.appendChild(list);
-}
-
-/*document.querySelector(".movies").textContent = JSON.stringify(json.movies);*/
-
-getMovies();
-
-/*function addMoviesToDom(challenge) {}
-
-async function getMovies() {
-  try {
-    let res = await fetch("src/filmer.json");
-
-    if (res.status !== 200) {
-      setError("Movies not found", "white");
-    } else {
-      let data = await res.json();
-      challenge = data.movies;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-getMovies();*/
+getJSON();
