b230ef784d98fbd1dd60dddf85079f4f3bb99f45
mkor92 <mkorhonen92@gmail.com>
2023-01-01 17:15:40 +0100

    all movies page completed with html,css and JS

--- src/JS/movies.js
+++ src/JS/movies.js
@@ -1,13 +1,12 @@
-const url = "src/movies.json";
 async function getMovies() {
-  const response = await fetch(url);
+  const response = await fetch("src/movies.json");
   const data = await response.json();
   const allMovies = data.movies;
 
   for (let i = 0; i < allMovies.length; i++) {
     const ul = document.querySelector(".movies-list");
     const li = document.createElement("li");
-    li.classList.add("movie");
+    li.classList.add("movies-list-item");
 
     li.innerHTML += `<img src= "${allMovies[i].image[0]}" alt="movie image" id="${allMovies[i].id}" />`;
     ul.appendChild(li);
