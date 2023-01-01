d440abf6471eef4b7f1a7a307e7b716542ee24cf
mkor92 <mkorhonen92@gmail.com>
2023-01-01 15:21:16 +0100

    get movies from json and render to all movies page

--- src/JS/movies.js
+++ src/JS/movies.js
@@ -1,7 +1,16 @@
-async function getJSON() {
-  const response = await fetch("src/movies.json");
-  const json = await response.json();
-  console.log(json);
-}
+const url = "src/movies.json";
+async function getMovies() {
+  const response = await fetch(url);
+  const data = await response.json();
+  const allMovies = data.movies;
 
-getJSON();
+  for (let i = 0; i < allMovies.length; i++) {
+    const ul = document.querySelector(".movies-list");
+    const li = document.createElement("li");
+    li.classList.add("movie");
+
+    li.innerHTML += `<img src= "${allMovies[i].image[0]}" alt="movie image" id="${allMovies[i].id}" />`;
+    ul.appendChild(li);
+  }
+}
+getMovies();
