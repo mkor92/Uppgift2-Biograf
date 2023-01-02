0afb5d19079e10d7ee20d6bcfbd7c85508b8cd2a
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-01-02 17:56:41 +0100

    Added simple styling and links to restaurant in different html pages

---
+++ src/JS/restaurant.js
@@ -0,0 +1,14 @@
+/*
+if( window.style.width <= "1000px" ) {
+    document.querySelector("li:first-child").removeAttribute.href = "/restaurant.html";
+}
+else { //when clicked to open menu add herf in mobile view 
+};
+
+
+
+if ( document.querySlector(".nav").display.flex-content === "column") {
+
+}
+
+*/
