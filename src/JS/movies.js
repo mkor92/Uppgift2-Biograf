1168c6caf4d8d7ad124aeaac9f0341777e62ffb8
mkor92 <mkorhonen92@gmail.com>
2023-01-03 15:23:37 +0100

    last small changes. Filtration now working

--- src/JS/movies.js
+++ src/JS/movies.js
@@ -6,31 +6,25 @@ export async function renderMovies() {
   const comedyCategory = document.querySelectorAll(".comedy-category");
   const actionCategory = document.querySelectorAll(".action-category");
   const dramaCategory = document.querySelectorAll(".drama-category");
-  const categoryItem = document.querySelectorAll(".movies-list-item");
-  const showAllMovies = document.querySelectorAll(".show-all-movies");
   const ul = document.querySelector(".movies-list");
 
-  showAllMovies.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      ul.innerHTML = "";
-      for (let i = 0; i < allMovies.length; i++) {
-        categoryHeading.innerHTML = "Alla filmer";
-        const li = document.createElement("li");
-        const category = allMovies[i].category;
-        const textOnImage = document.createElement("p");
-        li.classList.add("movies-list-item");
-        li.classList.add(category);
-        li.innerHTML += `<img src= "${allMovies[i].image[0]}" alt="movie image" id="${allMovies[i].id}" />`;
-        textOnImage.innerHTML = `<span>${category}</span>`;
-        ul.appendChild(li);
-        li.appendChild(textOnImage);
-      }
-    });
-  });
+  for (let i = 0; i < allMovies.length; i++) {
+    categoryHeading.innerHTML = "Alla filmer";
+    const li = document.createElement("li");
+    const category = allMovies[i].category;
+    const textOnImage = document.createElement("p");
+    li.classList.add("movies-list-item");
+    li.classList.add(category);
+    li.innerHTML += `<img src= "${allMovies[i].image[0]}" alt="movie image" id="${allMovies[i].id}" />`;
+    textOnImage.innerHTML = `<span>${category}</span>`;
+    ul.appendChild(li);
+    li.appendChild(textOnImage);
+  }
+
   comedyCategory.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
+
       ul.innerHTML = "";
       for (let i = 0; i < allMovies.length; i++) {
         if (allMovies[i].category == "Komedi") {
@@ -53,6 +47,7 @@ export async function renderMovies() {
   actionCategory.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
+
       ul.innerHTML = "";
       for (let i = 0; i < allMovies.length; i++) {
         if (allMovies[i].category == "Action") {
@@ -75,6 +70,7 @@ export async function renderMovies() {
   dramaCategory.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
+
       ul.innerHTML = "";
       for (let i = 0; i < allMovies.length; i++) {
         if (allMovies[i].category == "Drama") {
@@ -95,56 +91,3 @@ export async function renderMovies() {
   });
 }
 renderMovies();
-
-/*export async function movieCategory() {
-  const allMovies = await getMovies();
-  const categoryHeading = document.querySelector(".movies-h2");
-  const comedyCategory = document.querySelector(".comedy-category");
-  const actionCategory = document.querySelector(".action-category");
-  const dramaCategory = document.querySelector(".drama-category");
-  let categoryItem = document.querySelectorAll(".movies-list-item");
-
-  comedyCategory.addEventListener("click", () => {
-    categoryHeading.innerHTML = "Komedi";
-
-    for (let i = 0; i < categoryItem.length; i++) {
-      if (
-        categoryItem[i].classList.contains("Action") ||
-        categoryItem[i].classList.contains("Drama")
-      ) {
-        categoryItem[i].classList.add("hidden");
-      } else {
-        categoryItem[i].classList.remove("hidden");
-      }
-    }
-  });
-
-  actionCategory.addEventListener("click", () => {
-    categoryHeading.innerHTML = "Action";
-    for (let i = 0; i < categoryItem.length; i++) {
-      if (
-        categoryItem[i].classList.contains("Komedi") ||
-        categoryItem[i].classList.contains("Drama")
-      ) {
-        categoryItem[i].classList.add("hidden");
-      } else {
-        categoryItem[i].classList.remove("hidden");
-      }
-    }
-  });
-  dramaCategory.addEventListener("click", () => {
-    categoryHeading.innerHTML = "Drama";
-    for (let i = 0; i < categoryItem.length; i++) {
-      if (
-        categoryItem[i].classList.contains("Action") ||
-        categoryItem[i].classList.contains("Komedi")
-      ) {
-        categoryItem[i].classList.add("hidden");
-      } else {
-        categoryItem[i].classList.remove("hidden");
-      }
-    }
-  });
-}
-
-movieCategory();*/
