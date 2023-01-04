42cff8b14bfa7f6d875910193248c9ca1ec34abf
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-01-04 12:06:59 +0100

    location check before rendering movies on start page and deleted / from background image

--- src/JS/main.js
+++ src/JS/main.js
@@ -3,42 +3,44 @@ import { getMovies } from "./getMovies.js";
 
 mobileMenu();
 
-(async function () {
-  let moviesArray = await getMovies();
+if (location == "https://mkor92.github.io/Uppgift2-Biograf/") {
+  (async function () {
+    let moviesArray = await getMovies();
 
-  moviesArray
-    .filter((movie) => movie.category === "Komedi")
-    .slice(0, 3)
-    .forEach((item) => {
-      let movieItem = document.createElement("li");
-      let movieImg = document.createElement("img");
-      movieImg.src = item.image[0];
-      movieItem.classList.add("movies-list-item");
-      document.querySelector(".comedy-start-list").appendChild(movieItem);
-      movieItem.appendChild(movieImg);
-    });
+    moviesArray
+      .filter((movie) => movie.category === "Komedi")
+      .slice(0, 3)
+      .forEach((item) => {
+        let movieItem = document.createElement("li");
+        let movieImg = document.createElement("img");
+        movieImg.src = item.image[0];
+        movieItem.classList.add("movies-list-item");
+        document.querySelector(".comedy-start-list").appendChild(movieItem);
+        movieItem.appendChild(movieImg);
+      });
 
-  moviesArray
-    .filter((movie) => movie.category === "Action")
-    .slice(0, 3)
-    .forEach((item) => {
-      let movieItem = document.createElement("li");
-      let movieImg = document.createElement("img");
-      movieImg.src = item.image[0];
-      movieItem.classList.add("movies-list-item");
-      document.querySelector(".action-start-list").appendChild(movieItem);
-      movieItem.appendChild(movieImg);
-    });
+    moviesArray
+      .filter((movie) => movie.category === "Action")
+      .slice(0, 3)
+      .forEach((item) => {
+        let movieItem = document.createElement("li");
+        let movieImg = document.createElement("img");
+        movieImg.src = item.image[0];
+        movieItem.classList.add("movies-list-item");
+        document.querySelector(".action-start-list").appendChild(movieItem);
+        movieItem.appendChild(movieImg);
+      });
 
-  moviesArray
-    .filter((movie) => movie.category === "Drama")
-    .slice(0, 3)
-    .forEach((item) => {
-      let movieItem = document.createElement("li");
-      let movieImg = document.createElement("img");
-      movieImg.src = item.image[0];
-      movieItem.classList.add("movies-list-item");
-      document.querySelector(".drama-start-list").appendChild(movieItem);
-      movieItem.appendChild(movieImg);
-    });
-})();
+    moviesArray
+      .filter((movie) => movie.category === "Drama")
+      .slice(0, 3)
+      .forEach((item) => {
+        let movieItem = document.createElement("li");
+        let movieImg = document.createElement("img");
+        movieImg.src = item.image[0];
+        movieItem.classList.add("movies-list-item");
+        document.querySelector(".drama-start-list").appendChild(movieItem);
+        movieItem.appendChild(movieImg);
+      });
+  })();
+}
