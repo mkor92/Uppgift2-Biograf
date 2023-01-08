9b2b91d814ed02bfb2b3e104452f7b54fc9111b3
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-01-08 22:27:49 +0100

    Created navigation same as in movies site for salons sites. No hover effect. Changed on all html pages

--- src/JS/checkLocation.js
+++ src/JS/checkLocation.js
@@ -1,15 +1,18 @@
 export default function checkLocationForStyle() {
-
-    if (location.pathname === "/restaurant.html" || location.pathname === "/Uppgift2-Biograf/restaurant.html") {
-        document.querySelector("#restaurant-id").style.textDecoration = "underline";
-    }
-    
-    if (location.pathname === "/movies.html" || location.pathname === "/Uppgift2-Biograf/movies.html") {
-       document.querySelector("#movies-id").style.textDecoration = "underline";
-    }
-    
-    if (location.pathname === "/salons.html" || location.pathname === "/Uppgift2-Biograf/salons.html") {
-      document.querySelector("#salons-id").style.textDecoration = "underline";
-    }
-    
-    }
+  if (
+    location.pathname === "/restaurant.html" ||
+    location.pathname === "/Uppgift2-Biograf/restaurant.html"
+  ) {
+    document.querySelector("#restaurant-id").style.textDecoration = "underline";
+  } else if (
+    location.pathname === "/movies.html" ||
+    location.pathname === "/Uppgift2-Biograf/movies.html"
+  ) {
+    document.querySelector("#movies-id").style.textDecoration = "underline";
+  } else if (
+    location.pathname === "/salons.html" ||
+    location.pathname === "/Uppgift2-Biograf/salons.html"
+  ) {
+    document.querySelector("#salons-id").style.textDecoration = "underline";
+  } 
+}
