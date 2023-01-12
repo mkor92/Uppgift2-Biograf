05ca80550ead0eb94f6b968c544cd65a54199eff
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-01-12 02:41:58 +0100

    Sending in movie id with queryparams and rendering movie information, bad styling, help wanted

--- src/JS/movie-info.js
+++ src/JS/movie-info.js
@@ -1,12 +1,87 @@
-export async function movieInfo() {
+import { getMovies } from "./getMovies.js";
+
+export function movieInfo() {
   const movieOption = document.querySelectorAll(".movies-list-item");
   const description = document.querySelector(".description");
+  const movieImg = document.querySelectorAll(".movies-list-item img");
 
   for (let i = 0; i < movieOption.length; i++) {
-    movieOption[i].addEventListener("click", openMovieInfo, false);
-  }
+    movieOption[i].addEventListener("click", () => {
+      /*openMovieInfo, false);*/
+      let movieID = movieImg[i].id 
+      console.log(movieID)
+      window.open(`movie-info.html?movie-id=${movieID}`);
+      
+      
+
+  });
+}
+}
+
+  /*
   function openMovieInfo(e) {
     e.preventDefault();
     window.open("under-construction.html");
+  } */
+
+export async function findMovieID() {
+  let movieId = Number((location.search).slice(10));
+   console.log(movieId);
+
+   const moviesArray = await getMovies();
+
+  const filterdMovie = moviesArray.filter( item => movieId == item.id);
+  
+
+  const movie = filterdMovie[0];
+
+  console.log(movie);
+
+  document.querySelector(".bigger-image").src = movie.image[1];
+  document.querySelector(".movie-poster-small").src = movie.image[0];
+  document.querySelector(".movie-info-title").innerHTML = movie.title.toUpperCase();
+  document.querySelector(".category-info").innerHTML = movie.category;
+  document.querySelector(".movie-info-description").innerHTML = movie.description;
+  let screeningList = document.querySelector(".screening-dates");
+
+  const datesArray = movie.dateTime;
+
+  console.log(datesArray)
+
+    for (let i = 0; i < datesArray.length; i++) {
+    let screeningDate = document.createElement("li");
+    let bookingBtn = document.createElement("button");
+    screeningDate.innerHTML = new Date(movie.dateTime[i]).toLocaleDateString() + " kl " + new Date(movie.dateTime[i]).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+    screeningList.appendChild(screeningDate);
+    screeningDate.append(bookingBtn);
+    bookingBtn.innerHTML = "BILJETTER"
+    bookingBtn.classList.add("booking-button");
+    screeningDate.classList.add("screening-dates-item");
+    
   }
+
+
+
+
 }
+
+findMovieID();
+
+
+
+/* 
+      <section class="movie-information sec-cont">
+        <div class="images-container">
+          <img class="bigger-image" alt="bigger picture of the movie"/>
+          <img class="movie-poster-small" alt="movie poster" />
+          <h2 class="movie-info-title"></h2>
+          <p class="category-info"></p>
+        </div>
+        
+        <p class="movie-info-description"></p>
+        <h3 class="screenings">VISNINGSTIDER</h3>
+        <ul class="screening-dates"></ul>
+
+        
+      </section>
+*/
